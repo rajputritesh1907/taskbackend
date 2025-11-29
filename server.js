dbde755fff51bae386c348a68e26c535d7063623
Ritesh Singh <rajputritesh1907@gmail.com>
2025-11-29 10:10:28 +0530

    1st

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/projects', require('./routes/projectRoutes'));
 
+// Root route for health check
+app.get('/', (req, res) => {
+    res.json({ message: 'Backend API is running', status: 'OK' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
